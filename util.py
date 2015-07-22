b9461d3d004c9a144c60b7b108e7f605fb5b090a
supershdow <benjihofing@gmail.com>
2015-07-22 08:57:06 -0400

    SLOTH

--- util.py
+++ util.py
@@ -24,3 +24,10 @@ def toFahrenheit(K):
     C=K-273.15
     F=(C*9/5)+32
     return F
+
+def flickr(query='Sloth'):
+    url='https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=44b5c3dafd75b345edf09410ed6847e6&tags=%s&format=json&nojsoncallback=1&api_sig=63665d6d4dd9bfa2a9474cde2cd56a58'%query
+    urllib2.urlopen(url)
+    result=u.read()
+    w=json.loads(result)
+    return w['photos']['photo']
